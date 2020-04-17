260ace11ac4817e14e69d4f3349254a47f2fd3d0
rui-watanabe <wataru610806@gmail.com>
2020-04-17 19:09:20 +0900

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap-reboot.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/navbar.component';
-import ExercisesList from './components/exercises-list.component';
-import EditExercise from './components/edit-exercises.component';
-import CreateExercise from './components/create-exercise.component';
-import CreateUser from './components/create-user.component';
+// import ExercisesList from './components/exercises-list.component';
+// import EditExercise from './components/edit-exercises.component';
+// import CreateExercise from './components/create-exercise.component';
+// import CreateUser from './components/create-user.component';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <br/>
-      <Route path="/" exact component={ExercisesList} />
+      {/* <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <Route path="/user" component={CreateUser} /> */}
     </Router>
   );
 }
