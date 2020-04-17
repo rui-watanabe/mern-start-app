4ef28fcfd14eb13232af430346e5749855a3b2d5
rui-watanabe <wataru610806@gmail.com>
2020-04-17 18:27:18 +0900

    setting components App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap-reboot.min';
+
+import Navbar from './components/navbar.component';
+import ExercisesList from './components/exercises-list.component';
+import EditExercise from './components/edit-exercises.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
 
 function App() {
   return (
-    <div className="container">
-      hello world
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
