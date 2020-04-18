b542eb33366dba6f1ac5423770f042f6db4d2b60
rui-watanabe <wataru610806@gmail.com>
2020-04-18 18:59:11 +0900

    set initial data in create user component

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CreateUser = () => {
+const CreateUser = props => {
+  const [username, setUsername] = useState('');
+
+  const onChangeUsername = e => {
+    setUsername(e.target.value);
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const user = {
+      username
+    }
+    console.log(user);
+    window.location = "/";
+  }
+
   return(
     <div>
-      <p>You are on the Create User component</p>
+      <h3>Create New User</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+        <label>Username: </label>
+          <input 
+            type="text"
+            required
+            className="form-control"
+            value={username}
+            onChange={onChangeUsername}
+          />
+        </div>
+        <div className="form-grop">
+          <input type="submit" value="Create User" className="btn btn-primary" />
+        </div>
+      </form>
     </div>
   )
 };
-
 export default CreateUser;
